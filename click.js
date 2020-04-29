50c7c0a00781c20fda01ef8204a4ae1b1ac5e7be
Asuna Yuuki <the_real_asuna_yuuki@outlook.com>
2020-04-29 14:16:16 +0300

    h

--- click.js
+++ click.js
@@ -1,6 +1,10 @@
-
 document.getElementById('h').addEventListener('click', () => (
-	// No Semicolons Or This Is Dodo (Airlines)
-	console.log('This Works')
-  ));
+	// No END BRACKETS Or This Is Dodo
+	if (localStorage.clickCount) {
+		localStorage.clickCount = Number(localStorage.clickCount) + 1;
+	  } else {
+		localStorage.clickCount = 0;
+	  }
+	  document.getElementById("h-count").innerHTML = localStorage.clickCount + " time(s).";
+));
   

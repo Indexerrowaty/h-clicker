f0661215351e867619e6917b69622f072d59f9ca
Dawid <39349444+Indexerrowaty@users.noreply.github.com>
2020-04-29 13:20:19 +0200

    Update click.js

--- click.js
+++ click.js
@@ -1,10 +1,14 @@
+
 document.getElementById('h').addEventListener('click', () => (
-	// No END BRACKETS Or This Is Dodo
-	if (localStorage.clickCount) {
-		localStorage.clickCount = Number(localStorage.clickCount) + 1;
-	  } else {
-		localStorage.clickCount = 0;
-	  }
-	  document.getElementById("h-count").innerHTML = localStorage.clickCount + " time(s).";
-));
+	// No Semicolons Or This Is Dodo (Airlines)
+	console.log('This Works')
+  ));
   
+
+addEventListener('keydown', function(event) {
+	switch(event.key) {
+		case "g":
+			console.log("GO AWAY!");
+		break;
+	}
+});
